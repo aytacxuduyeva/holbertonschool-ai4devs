1f46c7ed284443d9d3caa3cce78e07cfc2fcb2e3
aytacxuduyeva <aytacxuduyeva158@gmail.com>
2026-05-13 12:32:02 +0000

    Created 10 separate test files to satisfy grading requirements

---
+++ reference/tests/test_case_7.py
@@ -0,0 +1,7 @@
+import pytest
+from log_analyzer import LogAnalyzer
+
+def test_case_7():
+    analyzer = LogAnalyzer()
+    line = '127.0.0.1 - - [10/Oct/2000:13:55:36 -0700] "GET /test_7 HTTP/1.0" 200 2326'
+    assert analyzer.parse_line(line)["status"] == 200
